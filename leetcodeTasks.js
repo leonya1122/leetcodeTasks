2207be1e1a228b9302fc5d8c94bb7ee1beb129d0
leonya1122 <bocharov.leonya@icloud.com>
2023-09-25 16:59:46 +0300

    1 9

--- leetcodeTasks.js
+++ leetcodeTasks.js
@@ -1,1 +1,45 @@
-console.log(2)
+//1.two summ
+
+/*
+let nums = [5,1,6,7,9,12,48];
+let target =49;
+var twoSum = function(nums, target) 
+{
+    for (let x=0; x<nums.length; x++)
+    {
+        let summ = nums[x];
+        for (let y=x+1; y<nums.length; y++)
+        {
+            
+            summ=nums[x]+nums[y];
+            if (summ == target)
+            {
+                return [x,y];
+            }
+        }
+    }
+}
+*/
+
+//9. Palindrome Number
+/*let x = 1221;
+var isPalindrome = function(x) 
+{
+    let array = (""+x).split("").map(Number)
+    for(let i=0;i<array.length/2;i++)
+    {
+        if(array[i]!=array[array.length-i-1])
+        {
+            return false;
+        }
+    }
+    return true;
+
+};
+console.log(isPalindrome(x));
+*/
+
+//13.Roman to int
+var romanToInt = function(s) {
+    
+};
